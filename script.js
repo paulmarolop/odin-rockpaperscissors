3376aecce19764dd79ecac0d5f6ac9783b74ab15
Paul Marolop <64996162+paulmarolop@users.noreply.github.com>
2025-06-03 13:39:16 +0700

    add basic ui

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 let humanScore = 0;
 let computerScore = 0;
 
+const resultDiv = document.getElementById('round-result');
+const scoreDiv = document.getElementById('score');
+const winnerDiv = document.getElementById('winner');
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach(button => button.addEventListener("click", () => playRound(button.id, getComputerChoice())));
+console.log(buttons);
+
 function getComputerChoice(){
     const computerChoice = Math.floor(Math.random() * 3);
     if(computerChoice === 0){
@@ -15,8 +23,7 @@ function getComputerChoice(){
 // console.log(getComputerChoice(Math.floor(Math.random() * 3)));
 
 function getHumanChoice(){
-    const userChoice = prompt("Rock, Paper, or Scissors?").toLowerCase();
-        if(userChoice === "paper"){
+    if(userChoice === "paper"){
         return "paper";
     }else if(userChoice === "scissors"){
         return "scissors";
@@ -31,38 +38,43 @@ function playRound(userChoice, computerChoice) {
     console.log("User: " + userChoice + " Computer: " + computerChoice);
 
     if(userChoice === computerChoice){
+        resultDiv.textContent = "Draw!";
         console.log("Draw!");
-    }else if(userChoice === "paper" && computerChoice === "rock"){
+    }else if((userChoice === 'rock' && computerChoice === 'scissors') ||
+    (userChoice === 'paper' && computerChoice === 'rock') ||
+    (userChoice === 'scissors' && computerChoice === 'paper')){
         humanScore++;
-        console.log("You win!");
-        console.log("Current Score - User: " + humanScore + " Computer: " + computerScore);
-    }else if(userChoice === "scissors" && computerChoice === "paper"){
-        humanScore++;
-        console.log("You win!");
-        console.log("Current Score - User: " + humanScore + " Computer: " + computerScore);
-    }else if(userChoice === "rock" && computerChoice === "scissors"){
-        humanScore++;
+        resultDiv.textContent = `You win! ${userChoice} beats ${computerChoice}`;
         console.log("You win!");
         console.log("Current Score - User: " + humanScore + " Computer: " + computerScore);
     }else{
         computerScore++;
         console.log("You lose!");
+        resultDiv.textContent = `You lose! ${computerChoice} beats ${userChoice}`;
         console.log("Current Score - User: " + humanScore + " Computer: " + computerScore);
+    }
+
+    scoreDiv.textContent = 'Player: ' + humanScore + ' Computer: ' + computerScore;
+
+    if(humanScore === 5){
+        winnerDiv.textContent = "You win the game!";
+    }else if(computerScore === 5){
+        winnerDiv.textContent = "You lose the game!";
     }
 }
 
-let rounds = 5;
+// let rounds = 5;
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 playRound(humanSelection, computerSelection);
 
-function playGame(rounds){
-    for(let i = 0; i < rounds; i++){
-        const humanSelection = getHumanChoice();
-        const computerSelection = getComputerChoice();
-        playRound(humanSelection, computerSelection);
-    }
-}
+// function playGame(rounds){
+//     for(let i = 0; i < rounds; i++){
+//         const humanSelection = getHumanChoice();
+//         const computerSelection = getComputerChoice();
+//         playRound(humanSelection, computerSelection);
+//     }
+// }
 
-playGame(rounds);
+// playGame(rounds);
 
